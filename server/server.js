abe3ba5ee607d7b4f752c31e5c761b2bea628154
Utsav-gajera <gajerautsav08@gmail.com>
2025-09-19 00:06:44 +0530

    Initial commit - job portal

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
+import "./config/instrument.js"
 import express from 'express';
 import cors from 'cors'
 import 'dotenv/config'
+import connectDB from './config/db.js';
+import * as Sentry from "@sentry/node";
+import { ClerkWebhook } from "./controllers/webhooks.js";
+
 
 
 // initialize express
 
 const app = express();
+
+//connect to database
+
+await connectDB();
 
 //middlewares
 
@@ -15,10 +24,18 @@ app.use(express.json());
 //routes
 
 app.get('/',(req,res)=>res.send('API is running'))
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+app.post('/webhooks',ClerkWebhook);
 
 //port
 
 const PORT = process.env.PORT || 5000;
+
+Sentry.setupExpressErrorHandler(app);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
